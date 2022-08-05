f7437007cd18e9df8d59598682217d77fdeb1627
WanDur <wandur933@gmail.com>
2022-08-05 19:29:02 +0800

    Update Launcher

--- launcher/Launcher.cpp
+++ launcher/Launcher.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
-#include<conio.h>
+#include <conio.h>
 
 int main()
 {
-	std::cout << "\tPress y to launch the bot." << std::endl;
-	std::cout << "\tor n to quit." << std::endl;
+	std::cout << "Make sure the game is opened.\n\n";
+	std::cout << "- 1 will launch the bot directly.\n- 2 will check game window then launch the bot.\n" << std::endl;
+	std::cout << "\t1) Launch the bot" << std::endl;
+	std::cout << "\t2) Launch bot with check" << std::endl;
+	std::cout << "\t3) Exit" << std::endl;
 
 	char choice = _getch();
 
-	if (choice == 'y')
+	if (choice == '1')
 	{
+		system("cls");
 		system("script\\main.py");
 	}
-	else if (choice == 'n')
+	else if (choice == '2')
+	{
+		system("cls");
+		int code = system("script\\test.py");
+
+		if (code == 0)
+		{
+			system("cls");
+			system("script\\main.py");
+		}
+	}
+	else if (choice == '3')
 	{
 		exit(0);
 	}
