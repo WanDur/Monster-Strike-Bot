8ffb41461a904219234d087b5152232b32d3d18e
WanDur <wandur933@gmail.com>
2022-11-15 11:30:06 +0800

    update launcher

--- launcher/Launcher.cpp
+++ launcher/Launcher.cpp
@@ -4,9 +4,9 @@
 int main()
 {
 	std::cout << "Make sure the game is opened.\n\n";
-	std::cout << "- 1 will launch the bot directly.\n- 2 will check game window then launch the bot.\n" << std::endl;
-	std::cout << "\t1) Launch the bot" << std::endl;
-	std::cout << "\t2) Launch bot with check" << std::endl;
+	std::cout << "- 1 will launch the bot directly.\n- 2 will launch the bot assuming shortcut is on. And reduce the time of waiting.\n" << std::endl;
+	std::cout << "\t1) Normal mode" << std::endl;
+	std::cout << "\t2) Speed mode" << std::endl;
 	std::cout << "\t3) Exit" << std::endl;
 
 	char choice = _getch();
@@ -19,13 +19,7 @@ int main()
 	else if (choice == '2')
 	{
 		system("cls");
-		int code = system("script\\test.py");
-
-		if (code == 0)
-		{
-			system("cls");
-			system("script\\main.py");
-		}
+		system("script\\main2.py");
 	}
 	else if (choice == '3')
 	{
